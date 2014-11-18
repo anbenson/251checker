7448d82b4cd263eceac4bf501eef60a11f9fbaa3
Andrew Benson <adbenson@cmu.edu>
2014-11-18 00:54:46 -0500

    reorganized eventPage.js, modularize, parse status codes, etc.

--- eventPage.js
+++ eventPage.js
@@ -1,19 +1,52 @@
-var pinger = {
-  online: false,
-  websiteToPing: "http://www.andrew.cmu.edu/course/15-251/grades/index.html",
-  notificationOnline: {
+// eventPage.js
+// Andrew Benson
+// description: code that works in the background at intervals to ping the grades website
+// and notify the user through Chrome notifications and UI changes
+
+// the NotificationManager handles all parts of UX i.e. notifications and icon changes
+var NotificationManager = {
+  onlineNotification: {
     type: "basic",
     title: "251 Grades Currently Online",
     message: "251 grades are accessible right now.",
     iconUrl: "online.png"
   },
-  notificationOffline: {
+  offlineNotification: {
     type: "basic",
     title: "251 Grades No Longer Online",
     message: "The website seems to have gone offline.",
     iconUrl: "offline.png"
   },
-  lastNotification: null,
+  nextNotificationId: 0,
+  notifyOnline: function() {
+    // change icon to online
+    chrome.browserAction.setIcon({
+      path: "online.png"
+    });
+    // send out notification
+    chrome.notifications.create(NotificationManager.nextNotificationId.toString(),
+                                NotificationManager.onlineNotification,
+                                function() {});
+    NotificationManager.nextNotificationId++;
+  },
+  notifyOffline: function() {
+    // change icon to offline
+    chrome.browserAction.setIcon({
+      path: "offline.png"
+    });
+    // send out notification
+    chrome.notifications.create(NotificationManager.nextNotificationId.toString(),
+                                NotificationManager.offlineNotification,
+                                function() {});
+    NotificationManager.nextNotificationId++;
+  }
+};
+
+// the WebsitePinger handles all details of figuring out if the website works, and whether
+// to notify the user
+var WebsitePinger = {
+  online: false,
+  websiteToPing: "http://www.andrew.cmu.edu/course/15-251/grades/index.html",
   setup: function() {
     this.checksite();
     chrome.alarms.create("myAlarm", {periodInMinutes: 0.1});
@@ -24,38 +57,28 @@ var pinger = {
       if (this.readyState !== 4) return;
       if (this.status === 0) return;
 
-      if (this.status === 404 && pinger.online) {
-        pinger.online = false;
-        chrome.browserAction.setIcon({
-          path: "offline.png"
-        });
-        if (!pinger.lastNotification) {
-          pinger.lastNotification = chrome.notifications.create("251notify", pinger.notificationOffline, function() {});
-        }
-        else {
-          chrome.notifications.update(pinger.lastNotification, pinger.notificationOffline, function() {});
-        }
+      var statusLevel = Math.floor(this.status / 100);
+      // if HTTP response code is a 400 or 500 level status
+      if ((statusLevel === 4 || statusLevel === 5) && WebsitePinger.online) {
+        WebsitePinger.online = false;
+        NotificationManager.notifyOffline();
       }
-
-      else if (this.status !== 404 && !pinger.online) {
-        pinger.online = true;
-        chrome.browserAction.setIcon({
-          path: "online.png"
-        });
-        if (!pinger.lastNotification) {
-          pinger.lastNotification = chrome.notifications.create("251notify", pinger.notificationOnline, function() {});
-        }
-        else {
-          chrome.notifications.update(pinger.lastNotification, pinger.notificationOnline, function() {});
-        }
+      // if HTTP response code is a 200 level status
+      else if (statusLevel === 2 && !WebsitePinger.online) {
+        WebsitePinger.online = true;
+        NotificationManager.notifyOnline();
       }
-    }
-    xhr.open("get", this.websiteToPing, true);
+      // unexpected status code?
+      else if (statusLevel !== 2 && statusLevel !== 4 && statusLevel !== 5) {
+        console.log("Unexpected HTTP status code: "+this.status.toString());
+      }
+    };
+    xhr.open("get", WebsitePinger.websiteToPing, true);
     xhr.send();
   }
-}
+};
 
-pinger.setup();
+WebsitePinger.setup();
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  pinger.checksite();
+  WebsitePinger.checksite();
 });
