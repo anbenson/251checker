411d04a7a0d1bde2399a75dc53542874854bbead
Andrew Benson <adbenson@cmu.edu>
2014-11-18 00:57:56 -0500

    added comment header

--- popup.js
+++ popup.js
@@ -1,1 +1,4 @@
+// popup.js
+// Andrew Benson
+// description: opens a new tab with the grades website. shouldn't be too hard to understand
 chrome.tabs.create({url: "https://www.andrew.cmu.edu/course/15-251/grades/index.html"});
